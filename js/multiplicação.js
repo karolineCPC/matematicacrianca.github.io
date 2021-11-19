22973ad13e87384281c8a70cc0c96ed997b4efc2
karoline cristina portal <karol26portal@gmail.com>
2021-11-19 13:21:55 -0400

    continuacao

--- js/multiplicação.js
+++ js/multiplicação.js
@@ -1,14 +1,14 @@
-const opção1 = document.getElementById("opção1"),
-const opção2 = document.getElementById("opção2"),
-const opção3 = document.getElementById("opção3"),
-const audio = document.getElementById("meuAudio"),
+var opcao1 = document.getElementById("opcao1");
+var opcao2 = document.getElementById("opcao2");
+var opcao3 = document.getElementById("opcao3");
+var audio = document.getElementById("meuAudio");
 var resposta = 0;
 
 function gerar_equação(){
-    var num1 = calculo.floor(calculo.random() * 12),
-        num2 = calculo.floor(calculo.random() * 12),
-        dummyResposta1 = calculo.floor(calculo.random() * 10),
-        dummyResposta2 = calculo.floor(calculo.random() * 10),
+    var num1 = Math.floor(Math.random() * 12),
+        num2 = Math.floor(Math.random() * 12),
+        dummyResposta1 = Math.floor(Math.random() * 10),
+        dummyResposta2 = Math.floor(Math.random() * 10),
         allRespostas = [],
         switchRespostas = [];
     resposta = eval (num1  * num2);
@@ -19,16 +19,16 @@ function gerar_equação(){
     allRespostas = [resposta, dummyResposta1, dummyResposta2];
 
     for (i = allRespostas.length; i--;){
-        switchRespostas.push(allRespostas.splice(calculo.floor(calculo.random() * (i + 1)), 1)[0]);
+        switchRespostas.push(allRespostas.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
     };
 
-    opção1.innerHTML = switchRespostas[0];
-    opção2.innerHTML = switchRespostas[1];
-    opção3.innerHTML = switchRespostas[2];
+    opcao1.innerHTML = switchRespostas[0];
+    opcao2.innerHTML = switchRespostas[1];
+    opcao3.innerHTML = switchRespostas[2];
 };
-
-opção1.addEventListener("click", function(){
-    if(opção1.innerHTML == resposta){
+console.log(opcao1)
+opcao1.addEventListener("click", function(){
+    if(opcao1.innerHTML == resposta){
         gerar_equação(); 
     }
     else{
@@ -36,8 +36,8 @@ opção1.addEventListener("click", function(){
     }
 });
 
-opção2.addEventListener("click", function(){
-    if(opção2.innerHTML == resposta){
+opcao2.addEventListener("click", function(){
+    if(opcao2.innerHTML == resposta){
         gerar_equação(); 
     }
     else{
@@ -45,8 +45,8 @@ opção2.addEventListener("click", function(){
     }
 });
 
-opção3.addEventListener("click", function(){
-    if(opção3.innerHTML == resposta){
+opcao3.addEventListener("click", function(){
+    if(opcao3.innerHTML == resposta){
         gerar_equação(); 
     }
     else{
